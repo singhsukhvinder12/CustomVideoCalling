df0e6ac3f5a1cbee6a505e06d834133a02e86105
SUKH E <ajay.seasia@outlook.com>
2020-08-22 15:35:12 +0530

    22-08-2020

--- app/src/main/java/com/customvideocalling/views/MainActivity.kt
+++ app/src/main/java/com/customvideocalling/views/MainActivity.kt
@@ -1,30 +1,30 @@
 package com.customvideocalling.views
 
 import android.content.Intent
+import android.graphics.Typeface
 import android.view.Gravity
 import android.view.View
+import android.widget.FrameLayout
+import android.widget.TextView
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
-import androidx.fragment.app.FragmentTransaction
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-import com.bumptech.glide.Glide
+import androidx.viewpager.widget.ViewPager.OnPageChangeListener
 import com.customvideocalling.R
 import com.customvideocalling.adapters.MyViewPagerAdapter
 import com.customvideocalling.application.MyApplication
+import com.customvideocalling.constants.GlobalConstants
 import com.customvideocalling.databinding.ActivityMainBinding
 import com.customvideocalling.utils.SharedPrefClass
 import com.customvideocalling.utils.core.BaseActivity
 import com.customvideocalling.viewmodels.MainViewModel
 import com.customvideocalling.views.authentication.LoginActivity
-import com.customvideocalling.views.fragment.HomeFragment
-import com.customvideocalling.views.fragment.JobRequestsFragment
-import com.customvideocalling.views.fragment.StudentHistoryFragment
 import com.customvideocalling.views.student.AddTokentActivity
 import com.customvideocalling.views.student.TokenHistoryActivity
 import com.google.android.material.navigation.NavigationView
 import com.google.android.material.tabs.TabLayout
-import kotlinx.android.synthetic.main.layout_custom_alert.*
+import org.w3c.dom.Text
 
 
 class MainActivity : BaseActivity() {
@@ -33,12 +33,24 @@ class MainActivity : BaseActivity() {
     private var navigationView: NavigationView? = null
     private var drawer: DrawerLayout? = null
     private var sharedPrefClass = SharedPrefClass()
+
+    var mIndicator: View? = null
+    private var indicatorWidth = 0
+
+    lateinit var tv_active:TextView
+    lateinit var tv_history:TextView
+
     override fun getLayoutId(): Int {
         return R.layout.activity_main
     }
 
     override fun initViews() {
+
+        tv_active=findViewById(R.id.tv_active)
+        tv_history=findViewById(R.id.tv_history)
+
         activityMainBinding = viewDataBinding as ActivityMainBinding
+        mIndicator = findViewById(R.id.indicator);
 
         navigationView = activityMainBinding!!.navView
         navigationView!!.alpha = 0.9f
@@ -59,16 +71,61 @@ class MainActivity : BaseActivity() {
             MyViewPagerAdapter(this, supportFragmentManager, activityMainBinding!!.tabs.tabCount)
         activityMainBinding!!.pager.adapter = adapter
 
+
+//        activityMainBinding!!.tabs.post(Runnable {
+//            indicatorWidth =  activityMainBinding!!.tabs.getWidth() /  activityMainBinding!!.tabs.getTabCount()
+//
+//            //Assign new width
+//            val indicatorParams =
+//                mIndicator!!.getLayoutParams() as FrameLayout.LayoutParams
+//            indicatorParams.width = indicatorWidth
+//            mIndicator!!.setLayoutParams(indicatorParams)
+//        })
+
         activityMainBinding!!.pager.addOnPageChangeListener(
             TabLayout.TabLayoutOnPageChangeListener(
                 activityMainBinding!!.tabs
             )
         )
 
+        activityMainBinding!!.pager.addOnPageChangeListener(object : OnPageChangeListener {
+            //To move the indicator as the user scroll, we will need the scroll offset values
+            //positionOffset is a value from [0..1] which represents how far the page has been scrolled
+            //see https://developer.android.com/reference/android/support/v4/view/ViewPager.OnPageChangeListener
+            override fun onPageScrolled(
+                i: Int,
+                positionOffset: Float,
+                positionOffsetPx: Int
+            ) {
+              /*  val params =
+                    mIndicator!!.getLayoutParams() as FrameLayout.LayoutParams
+
+                //Multiply positionOffset with indicatorWidth to get translation
+                val translationOffset = (positionOffset + i) * indicatorWidth
+                params.leftMargin = translationOffset.toInt()
+                mIndicator!!.setLayoutParams(params)*/
+
+
+            }
+
+            override fun onPageSelected(i: Int) {}
+            override fun onPageScrollStateChanged(i: Int) {}
+        })
+
+
+
         activityMainBinding!!.tabs.addOnTabSelectedListener(object :
             TabLayout.OnTabSelectedListener {
             override fun onTabSelected(tab: TabLayout.Tab) {
                 activityMainBinding!!.pager.currentItem = tab.position
+
+                if (tab.position==0){
+                    changeBackgroundAndText(tv_active,tv_history,"1")
+                }else  if (tab.position==1){
+                    changeBackgroundAndText(tv_active,tv_history,"2")
+                }else{
+
+                }
             }
 
             override fun onTabUnselected(tab: TabLayout.Tab) {
@@ -111,7 +168,7 @@ class MainActivity : BaseActivity() {
                         showToastSuccess("Coming Soon")
                     }
 
-                    "ll_profile" -> {
+                    "ivProfile" -> {
                         val intent = Intent(this, ProfileActivity::class.java)
                         startActivity(intent)
                     }
@@ -148,11 +205,12 @@ class MainActivity : BaseActivity() {
                              .load(image)
                              .placeholder(R.drawable.user)
                              .into(activityDashboardBinding!!.icProfile)*/
-                        val name = SharedPrefClass().getPrefValue(
+                        /*val name = SharedPrefClass().getPrefValue(
                             MyApplication.instance.applicationContext,
                             getString(R.string.first_name)
-                        )
-                        activityMainBinding!!.tvName.text = name.toString()
+                        )*/
+
+
                         if (drawer!!.isDrawerOpen(GravityCompat.START)) {
                             drawer!!.closeDrawer(Gravity.LEFT) //CLOSE Nav Drawer!
                         } else {
@@ -171,11 +229,53 @@ class MainActivity : BaseActivity() {
             })
         )
 
+        tv_active.setOnClickListener {
+            activityMainBinding!!.pager.setCurrentItem(0)
+            changeBackgroundAndText(tv_active,tv_history,"1")
+        }
+  tv_history.setOnClickListener {
+      activityMainBinding!!.pager.setCurrentItem(1)
+            changeBackgroundAndText(tv_active,tv_history,"2")
+        }
+        changeBackgroundAndText(tv_active,tv_history,"1")
 
         // replaceFragment()
     }
 
 
+    override fun onResume() {
+        super.onResume()
+        val name = SharedPrefClass().getPrefValue(
+            MyApplication.instance.applicationContext,
+            GlobalConstants.USERNAME
+        )
+        activityMainBinding!!.tvName.text = name.toString()
+    }
+
+    fun changeBackgroundAndText(text:TextView,text2:TextView,status:String){
+
+        if (status.equals("1")){
+            text.setTextColor(resources.getColor(R.color.white))
+            text.setBackgroundResource( R.drawable.shape_left_round_yellow)
+            text.setTypeface(text.getTypeface(), Typeface.NORMAL);
+
+            text2.setTextColor(resources.getColor(R.color.custom_blue))
+            text2.setBackgroundResource( R.drawable.shape_right_round_yellow_stroke)
+            text2.setTypeface(text2.getTypeface(), Typeface.BOLD);
+
+        }else if (status.equals("2")){
+            text.setTextColor(resources.getColor(R.color.custom_blue))
+            text.setBackgroundResource(  R.drawable.shape_left_round_yellow_stroke)
+            text.setTypeface(text.getTypeface(), Typeface.BOLD);
+
+
+            text2.setTextColor(resources.getColor(R.color.white))
+            text2.setBackgroundResource( R.drawable.shape_right_round_yellow)
+            text2.setTypeface(text2.getTypeface(), Typeface.NORMAL);
+
+        }
+
+    }
     /* fun replaceFragment(){
          val ft: FragmentTransaction = supportFragmentManager.beginTransaction()
          ft.replace(R.id.frame_layout, StudentHistoryFragment(), "")
